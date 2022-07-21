3a0a2bcf620311a14b9018d5bfc0b926f95e1080
EbadAlam <m.ebadalam@gmail.com>
2022-07-21 12:12:42 +0500

    Heroku update

--- config/database.php
+++ config/database.php
@@ -48,8 +48,8 @@ return [
             'url' => env('DATABASE_URL'),
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
+            'database' => env('DB_DATABASE', 'scd-test-final'),
+            'username' => env('DB_USERNAME', 'root'),
             'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
@@ -65,12 +65,14 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'url' => env('DATABASE_URL','postgres://nglhvgyfvemetb:ced472070084878e4137a9edc315f16f1bf06cdca55ad56d8c117e5854e1a94b@ec2-54-87-179-4.compute-1.amazonaws.com:5432/d6oggss7qbq3kr
+'),
+            'host' => env('DB_HOST', 'ec2-54-87-179-4.compute-1.amazonaws.com
+'),
             'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'd6oggss7qbq3kr'),
+            'username' => env('DB_USERNAME', 'nglhvgyfvemetb'),
+            'password' => env('DB_PASSWORD', 'ced472070084878e4137a9edc315f16f1bf06cdca55ad56d8c117e5854e1a94b'),
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
